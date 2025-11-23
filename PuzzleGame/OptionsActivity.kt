e9d74b9de83d12bbb0183d6ee3594379a55c4442
Ann <venerologbogat@mail.ru>
2025-11-23 23:36:16 +0300

    Update OptionsActivity.kt
    Switching between screens in settings (where authorship is indicated) + trial use of the back button

--- PuzzleGame/OptionsActivity.kt
+++ PuzzleGame/OptionsActivity.kt
@@ -1,1 +1,20 @@
-//настройки avtorstvo i td i tp//
+//настройки avtorstvo i td i tp + poprobyiy knopky vosvrata zdelat//
+package com.example.puzzle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_options.*
+
+class OptionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_options)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
