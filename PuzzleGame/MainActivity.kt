4f940b22f356e19193173c3511eb2bdf7ad3c304
Ann <venerologbogat@mail.ru>
2025-11-23 23:32:56 +0300

    Update MainActivity.kt
    Main screen and transition to level selection

--- PuzzleGame/MainActivity.kt
+++ PuzzleGame/MainActivity.kt
@@ -1,1 +1,22 @@
 //тут будет код//
+package com.example.puzzle
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_play.setOnClickListener {
+            startActivity(Intent(this, LevelsActivity::class.java))
+        }
+
+        btn_options.setOnClickListener {
+            startActivity(Intent(this, OptionsActivity::class.java))
+        }
+    }
+}
