b4b51a756d0fefdde0c020f12ba3bacf6ef4c2c0
Ann <venerologbogat@mail.ru>
2025-11-24 17:10:45 +0300

    Update OptionsActivity.kt
    Button back addition

--- PuzzleGame/OptionsActivity.kt
+++ PuzzleGame/OptionsActivity.kt
@@ -1,20 +1,21 @@
 //настройки avtorstvo i td i tp + poprobyiy knopky vosvrata zdelat//
-package com.example.puzzle
+package com.example.puzzlegame
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_options.*
 
 class OptionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
 
+        // knopka nazad v ActionBar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    // drygoii sposob obrabotki knopki "Nazad"
     override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
+        onBackPressedDispatcher.onBackPressed()
         return true
     }
 }
