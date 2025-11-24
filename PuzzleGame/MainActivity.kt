39839a12ee09d1d001c8f297647d35a472ce4061
Ann <venerologbogat@mail.ru>
2025-11-24 17:01:36 +0300

    Update MainActivity.kt
    Code addition

--- PuzzleGame/MainActivity.kt
+++ PuzzleGame/MainActivity.kt
@@ -1,21 +1,24 @@
-//тут будет код//
+//тут будет код, Dopystim kod//
 package com.example.puzzle
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_play.setOnClickListener {
+        // Naxodim knopki po ID
+        val btnPlay = findViewById<Button>(R.id.btn_play)
+        val btnOptions = findViewById<Button>(R.id.btn_options)
+
+        btnPlay.setOnClickListener {
             startActivity(Intent(this, LevelsActivity::class.java))
         }
 
-        btn_options.setOnClickListener {
+        btnOptions.setOnClickListener {
             startActivity(Intent(this, OptionsActivity::class.java))
         }
     }
